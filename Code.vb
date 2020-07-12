6956e1b191c853478c52dba65a20df8edec8ec9b
joshface-uk <54547819+joshface-uk@users.noreply.github.com>
2020-07-12 15:18:38 +0100

    Update Code.vb

--- Code.vb
+++ Code.vb
@@ -1,35 +1,23 @@
 Imports System.Data.OleDb
-
-Public Class LoginActivity
-
-    Private Sub LoginActivity_Load(sender As Object, e As EventArgs) Handles Me.Load
-        LoadAllLogonActivity()
-    End Sub
-
-    Private Sub LoadAllLogonActivity()
-        listview_AllLogonActivity.Items.Clear()
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim connection As OleDbConnection
         Dim command As OleDbCommand
         Dim data_reader As OleDbDataReader
-        connection = New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; Data Source=S:\Data\Application\.data\.secure\LogonActivityLog.accdb")
+        connection = New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; Data Source=C:\Users\Josh\Desktop\Users.accdb")
         connection.Open()
-        command = New OleDbCommand("SELECT * FROM LoginActivityLog", connection)
+        command = New OleDbCommand("SELECT * FROM Users", connection)
         data_reader = command.ExecuteReader
 
         If data_reader.HasRows Then
             While data_reader.Read
-                Dim NewItem As New ListViewItem()
-                NewItem.Text = data_reader.GetValue(1) 'Date
-                NewItem.SubItems.Add(data_reader.GetValue(2)) 'Time
-                NewItem.SubItems.Add(data_reader.GetValue(3)) 'Username
-                NewItem.SubItems.Add(data_reader.GetValue(4)) 'Success/Fail
-                NewItem.SubItems.Add(data_reader.GetValue(5)) 'Message
-                listview_AllLogonActivity.Items.Add(NewItem)
+                Dim NewItem As New ListViewItem
+                NewItem.Text = data_reader.GetValue(1) 'Forename
+                NewItem.SubItems.Add(data_reader.GetValue(2)) 'Surname
+                NewItem.SubItems.Add(data_reader.GetValue(3)) 'Age
+                NewItem.SubItems.Add(data_reader.GetValue(4)) 'Gender
+                ListView1.Items.Add(NewItem)
             End While
         End If
-    End Sub
-
-    Private Sub UpdateData_Tick(sender As Object, e As EventArgs) Handles UpdateData.Tick
-        LoadAllLogonActivity()
     End Sub
 End Class
